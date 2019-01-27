81df7472686a9a587f09f19e517576e2ed80a4f8
somerset <somerset@somersets-MacBook-Pro.local>
2019-01-27 15:01:32 -0800

    First Commit for MEAN project

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express = require('express'),
+app = express(),
+bodyParser = require('body-parser'),
+path = require('path');
+
+app.use(express.static(__dirname+'/public/dist/public'));
+app.use(bodyParser.json());
+
+require('./routes.js')(app);
+ 
+
+app.listen(8000, function() {
+console.log("listening on port 8000");
+})
